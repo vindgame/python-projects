b2483a37b552356f312e64f12d06dfeb0b2aea76
vindgame <67763433+vindgame@users.noreply.github.com>
2021-05-02 14:23:24 +0530

    Add files via upload

---
+++ even or odd numbers.py
@@ -0,0 +1,8 @@
+x =int(input("Enter an number : "))
+if x%2==0:
+     print(x, "Number is even")
+else:
+     print(x, "Number is odd")
+
+
+print()
---
+++ tables.py
@@ -0,0 +1,8 @@
+print ( "**** welcome to tables **** ")
+
+x=int(input( "Enter an number : "))
+
+for i in range(1,11):
+   print(x, "x", i , "=" ,i*x)
+   
+
